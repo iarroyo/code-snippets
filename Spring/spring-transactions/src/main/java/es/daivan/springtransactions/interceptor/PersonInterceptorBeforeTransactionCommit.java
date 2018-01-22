39ad126f721347bc51876e756c7957df9ee97109
Ivan Arroyo Escobar <iarroyoescobar@Ivans-MacBook-Pro.local>
2018-01-23 00:37:08 +0100

    Removed Order annotation

--- Spring/spring-transactions/src/main/java/es/daivan/springtransactions/interceptor/PersonInterceptorBeforeTransactionCommit.java
+++ Spring/spring-transactions/src/main/java/es/daivan/springtransactions/interceptor/PersonInterceptorBeforeTransactionCommit.java
@@ -3,11 +3,9 @@ package es.daivan.springtransactions.interceptor;
 import es.daivan.springtransactions.domain.Person;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Order
 @Component
 public class PersonInterceptorBeforeTransactionCommit {
 
